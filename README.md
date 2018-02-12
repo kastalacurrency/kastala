a62286794c436bbe124b2a001b06e4027f862103
kastalacurrency <kastalacurrency@protonmail.com>
2018-02-12 01:40:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# kastala
+# kastala
---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,25 @@
+// Copyright (c) 2012-2017, The CryptoNote developers, The Bytecoin developers
+//
+// This file is part of Bytecoin.
+//
+// Bytecoin is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Bytecoin is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Bytecoin.  If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid BYTECOIN_NETWORK = { {  0xb1, 0x6c, 0xa5, 0xac, 0x76, 0xc6, 0xaf, 0x72, 0x4b, 0x7d, 0x53, 0x78, 0x3b, 0x3f, 0x70, 0xd0  } };
+
+}
+
